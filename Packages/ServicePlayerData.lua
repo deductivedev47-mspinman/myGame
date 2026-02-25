aa9c9c4b90052e85a7838715b2d540cf650a792e
Nate Dev <deductivedev47@gmail.com>
2026-02-25 20:33:53 +0800

    added some libraries commit

---
+++ Packages/ServicePlayerData.lua
@@ -0,0 +1,3 @@
+local REQUIRED_MODULE = require(script.Parent._Index["leifstout_serviceplayerdata@0.1.2"]["serviceplayerdata"])
+export type ServicePlayerData<T> = REQUIRED_MODULE.ServicePlayerData<T>
+return REQUIRED_MODULE
